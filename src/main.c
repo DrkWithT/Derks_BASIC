2531350b6be2ccc87f7323cd2417aeae440c4c6e
drkwitht <derektan3510@gmail.com>
2025-05-29 00:18:53 -0700

    Updated build file, tests.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include "frontend/token.h"
+#include "frontend/lexer.h"
+#include "utils/safestr.h"
+
+static const char* fake_code = "# hello world #\n"
+"DEFUN main WITH args DO\n"
+"    PRINT \"Hello World!\"\n"
+"END\n";
+
+[[nodiscard]] bool test_lexing(const char* source) {
+    SafeStr source_str = safe_str_create_from(source);
+
+    if (!safe_str_valid(&source_str)) {
+        perror("test_lexing: failed to allocate safe string!\n");
+        safe_str_destroy(&source_str);
+        return false;
+    }
+
+    LexerState lexer = lexer_create(&source_str);
+    Token temp;
+
+    do {
+        temp = lexer_next(&lexer);
+
+        if (temp.tag == unknown) {
+            fprintf(stderr, "Lexical error at source [%d:%d], unknown token!\n", temp.line, temp.col);
+            return false;
+        }
+    } while (temp.tag != eof);
+
+    lexer_destroy(&lexer);
+    safe_str_destroy(&source_str);
+    return true;
+}
 
 int main() {
-    puts("Hello World!\n");
+    if (!test_lexing(fake_code)) {
+        return 1;
+    }
 }
